993cc26ec756d4156c0ee6c776ed89d5173d2d6f
Taguchi-wq <20cm0123@jec.ac.jp>
2021-07-31 16:10:39 +0900

    first commit

---
+++ P3Splashtutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  P3Splashtutorial
+//
+//  Created by cmStudent on 2021/07/31.
+//  Copyright © 2021 cmStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
